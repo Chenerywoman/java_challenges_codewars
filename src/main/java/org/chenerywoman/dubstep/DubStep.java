856ca0ebe8317540d6401410d1f1151dc12da7a0
Chenerywoman <rachelmchenery@hotmail.com>
2021-04-12 21:06:17 +0100

    refactoring into one line

--- src/main/java/org/chenerywoman/dubstep/DubStep.java
+++ src/main/java/org/chenerywoman/dubstep/DubStep.java
@@ -6,9 +6,7 @@ import java.util.StringJoiner;
 public class DubStep {
     public static String SongDecoder (String song)
     {
-        String withoutWubs = song.replaceAll("WUB", " ").trim();
-        String removeExtraSpaces = withoutWubs.replaceAll("\\s+", " ");
 
-        return removeExtraSpaces;
+        return song.replaceAll("WUB", " ").trim().replaceAll("\\s+", " ");
     }
 }
