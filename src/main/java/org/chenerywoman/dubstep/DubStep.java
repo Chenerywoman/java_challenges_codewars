179f8a7de158d976d2901ebd71b4e72e8f36ceb6
Chenerywoman <rachelmchenery@hotmail.com>
2021-04-12 20:55:36 +0100

    replacing string joiner with another replaceAll method to remove extra whitespace & passing tests

--- src/main/java/org/chenerywoman/dubstep/DubStep.java
+++ src/main/java/org/chenerywoman/dubstep/DubStep.java
@@ -7,16 +7,8 @@ public class DubStep {
     public static String SongDecoder (String song)
     {
         String withoutWubs = song.replaceAll("WUB", " ").trim();
-        String[] arr = withoutWubs.split(" ");
-        StringJoiner joiner = new StringJoiner("");
+        String removeExtraSpaces = withoutWubs.replaceAll("\\s+", " ");
 
-        for(int i = 0; i < arr.length; i++) {
-
-            if (arr[i] != " "){
-                joiner.add(arr[i]);
-            }
-        }
-
-        return  joiner.toString();
+        return removeExtraSpaces;
     }
 }
